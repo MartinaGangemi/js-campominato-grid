a5fa24359dcc07b68c7a8d18dbedd145fd291e2a
MartinaGangemi <martinagangemi94@gmail.com>
2022-04-04 16:56:26 +0200

    funzione per generare le celle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,48 @@
 document.getElementById("play").addEventListener("click", play);
 
 function play (){
+document.getElementById("cells").innerHTML = '';
 let difficoltà = document.getElementById("difficoltà").value;
 console.log(difficoltà)
 
 // creo delle condizioni in base alla difficoltà
 
 if (difficoltà == "facile"){
-    gridEasyMode("cells", "div", "cell_easy_mode")
+    createGrid("cells", "div", "cell_easy_mode", 100)
     
     } else if(difficoltà == "media"){
-        gridNormalMode("cells", "div", "cell_normal_mode")
+        createGrid("cells", "div", "cell_normal_mode", 81)
+    } else {
+        createGrid("cells", "div", "cell_hard_mode", 49)
     }
 
+    
 }
 
 
-
-// creo la funzione per generare le celle easy mode
-
-function gridEasyMode(selector, tagName, className){
+// creo la funzione per generare le celle
+function createGrid(selector, tagName, className, numeroDifficoltà){
     const cellsElement = document.getElementById(selector)
 
     // ciclo per generare tot quadratini
-    for (let i = 1; i<=100; i ++){
+    for (let i = 1; i<=numeroDifficoltà; i++){
     const divTag = document.createElement(tagName)
     divTag.classList.add(className)
     cellsElement.append(divTag)
     } 
 }
 
-// creo la funzione per generare le celle normal mode
+//funzione quando clicco sul quadrato si colora
 
-function gridNormalMode(selector, tagName, className){
-    const cellsElement = document.getElementById(selector)
 
-    // ciclo per generare tot quadratini
-    for (let i = 1; i<=81; i ++){
-    const divTag = document.createElement(tagName)
-    divTag.classList.add(className)
-    cellsElement.append(divTag)
-    } 
+function coloredcells (lenght){
+    const allCells= document.querySelectorAll(".cells")
+        for(let index=0; index < length ; index++){
+            const singlecell = cells[index]
+            console.log(singlecell)
+                
+        }
+
 }
+
 
